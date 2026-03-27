e378fd9e00e9f1e30e663d30f992e29c071f37b7
meghagautam23 <145484831+meghagautam23@users.noreply.github.com>
2026-03-27 09:24:59 +0530

    Update Main.java

--- src/LabCat/Main.java
+++ src/LabCat/Main.java
@@ -4,23 +4,16 @@ import java.util.Scanner;
 
 public class Main {
 public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
 
-System.out.print("Enter marks of Subject 1: ");
-int s1 = sc.nextInt();
 
-System.out.print("Enter marks of Subject 2: ");
-int s2 = sc.nextInt();
-
-System.out.print("Enter marks of Subject 3: ");
-int s3 = sc.nextInt();
+int s1 = 80;
+int s2 = 70;
+int s3 = 90;
 
 int total = s1 + s2 + s3;
 double avg = total / 3.0;
 
-System.out.println("Total Marks: " + total);
-System.out.println("Average: " + avg);
-
+  
 if (avg >= 90)
 System.out.println("Grade: A");
 else if (avg >= 75)
